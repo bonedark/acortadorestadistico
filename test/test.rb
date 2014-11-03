772f4ede67a7cc4b7f049674e2bbd83dde0329e4
jonathan1985 <alu0100450187@ull.edu.es>
2014-11-03 09:01:08 +0000

    configurando test

--- test/test.rb
+++ test/test.rb
@@ -1,65 +1,31 @@
 ENV['RACK_ENV'] = 'test'
-
 require 'minitest/autorun'
 require 'rack/test'
-require_relative '../twitter.rb'
 
+require 'bundler/setup'
+require 'sinatra'
+require 'data_mapper'
+require_relative '../app'
 
-include Rack::Test::Methods
+describe "shortened urls" do
 
-def app
-    Sinatra::Application
-end
+	before :all do
+		@user = "0000"
+		@email = "test@email.es"
+		@url = "http://www.google.es"
+		@urlshort = "google"
+		@ip = "88.10.68.232"
+		@country = "SPAIN"
+		@countryCode = "SP"
+		@city = "santa cruz"
+		@latitude = "0000"
+		@longitude = "1111"
 
-describe "SYTW P2 - page" do
-    
-    
-    it "Should return index" do
-        get '/'
-        assert last_response.ok?
-    end
-    
-    it "should return title" do
-        get '/'
-        assert_match "<title>SYTW - P2 - Amigos Populares en Twitter</title>", last_response.body
-    end
-    
-    it "should return form" do
-        get '/'
-        assert_match "<p ><b>Introduzca su nombre en Twitter: </b></p>", last_response.body
-        assert_match "<p ><b>¿Cuántos seguidores desea ver? </b></p>", last_response.body
-    end
-
-
-    it "Debe cargar la imagen del pajarito" do
-	get '/'
-	assert_match '<img src="/bird_icon.png" WIDTH=100 HEIGHT=100 />', last_response.body
-    end
-
-    it "should return foot" do
-        get '/'
-        assert_match "<p><b>SYTW - Práctica 2 Realizada por:</b><h1>-> Daniel Nicolás Fernández del Castillo Salazar</h1><h1>-> Jonathan Barrera Delgado</h1></p>", last_response.body
-    end
-    
-    #it "test_styles"
-    #    get "/public/css/estilo.css"
-    #   assert last_response.ok?
-    #end
-end
-
-describe "SYTW P2 - Funtions" do
-    before :all do
-        @tw = Twitterpopular.new
-        @client = my_twitter_client()
-        @name1 = "b0nedark"
-        @name2 = "bosdadanedark"
-    end
-    
-    it "the user exist" do
-        assert @tw.dusuario(@client,@name1)
-    end
-    
-    it "the user not exist" do
-        refute @tw.dusuario(@client, @name2)
-    end
+		@short_url = Shortenedurl.first_or_create(:uid => @user, :email => @email, :url => @url, :urlshort => @urlshort, :n_visits => 0)
+		@short_url1 = Shortenedurl.first(:urlshort => @urlshort)
+		@short_url.n_visits += 1
+		@short_url.save
+  		@visit = Visit.new(:ip => @ip, :country => @country, :countryCode => @countryCode, :city => @city, :latitude => @latitude, :longitude => @longitude, :shortenedurl => @short_url1, :created_at => Time.now)
+  		@visit.save
+	end
 end
